ba73b7120118b03d907d11e4e51702f2e254bd06
phuongnam8290 <phuongnam8290@gmail.com>
2022-10-10 03:31:39 +0700

    build: change dependencies & mapping for AWS deployment
    - Downgrade JSTL from 2.0 to 1.2.6 to conform with Tomcat 8.5.
    - Remove mapping to root url, using welcome file instead.

--- src/main/java/com/namdp/starmobile/controllers/HomeController.java
+++ src/main/java/com/namdp/starmobile/controllers/HomeController.java
@@ -1,9 +1,9 @@
 package com.namdp.starmobile.controllers;
 
-import com.namdp.starmobile.entities.User;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
--- src/main/java/com/namdp/starmobile/controllers/LoginController.java
+++ src/main/java/com/namdp/starmobile/controllers/LoginController.java
@@ -1,9 +1,12 @@
 package com.namdp.starmobile.controllers;
 
 import com.namdp.starmobile.entities.User;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
